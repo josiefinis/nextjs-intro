277feec5ce19a67bb208244e2cbb80157cfa05e7
Josefin Wall <josefin1010wall@gmail.com>
2026-07-07 13:12:06 +0200

    Create hero component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main>
-      {/* Hero section */}
-      <section className="relative">
-        <header className="relative z-10">
-          <h1 className="font-extrabold text-9xl py-1 mbe-1 font-display">
-            Nextjs Intro
-          </h1>
-        </header>
-        <Image
-          className="w-full h-auto object-cover z-0"
-          src={"/pink-sky.jpg"}
-          fill
-          alt=""
-          loading="eager"
-        />
-      </section>
+      <Hero />
     </main>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section className="relative">
+      <header className="relative z-10">
+        <h1 className="font-extrabold text-9xl py-1 mbe-1 font-display">
+          Nextjs Intro
+        </h1>
+      </header>
+      <Image
+        className="w-full h-auto object-cover z-0"
+        src={"/pink-sky.jpg"}
+        fill
+        alt=""
+        loading="eager"
+      />
+    </section>
+  );
+}
