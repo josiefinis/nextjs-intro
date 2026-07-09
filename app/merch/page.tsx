85eaf4170cfe53a2b2770b043be884ce8bc5f894
Josefin Wall <josefin1010wall@gmail.com>
2026-07-09 17:00:16 +0200

    Add image from external url.

--- app/merch/page.tsx
+++ app/merch/page.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 export default function MerchPage() {
   return (
-    <main>
-      <h1>Merch</h1>
+    <main className="flex flex-col items-center *:mx-4 *:md:mx-12">
+      <h1 className="text-fluid-4xl font-display">Merch</h1>
+      <article
+        aria-labelledby="time-and-death"
+        className="flex flex-col items-center"
+      >
+        <h2 className="font-display text-fluid-2xl" id="time-and-death">
+          Time and Death
+        </h2>
+        <figure>
+          <Image
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/1/1f/Time_and_Death.jpg"
+            }
+            alt="Illustration of a skull atop an hourglass with outspread wings and a snake entwined."
+            width={960}
+            height={600}
+          />
+          <figcaption className="font-display text-fluid-lg">
+            Illustration by Edmund J. Sullivan from <i>Sartor Resartus</i> by
+            Thomas Carlyle. 1898
+          </figcaption>
+        </figure>
+      </article>
     </main>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://upload.wikimedia.org/wikipedia/commons/1/1f/Time_and_Death.jpg",
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
