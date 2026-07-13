cffbfc6817250075bb369e3db7db43aab9320b02
Josefin Wall <josefin1010wall@gmail.com>
2026-07-13 16:14:12 +0200

    Create utility function to get event schedule.
    - Parse schedule data and return an array of Date objects.

--- lib/util.ts
+++ lib/util.ts
@@ -3,3 +3,12 @@ import data from "@/data/events.json";
 export function getEventById(id: string) {
   return data.results.find((event) => event.id === id);
 }
+
+export function getSchedule(id: string): Date[] {
+  const event = data.results.find((event) => event.id === id);
+  return (
+    event?.schedule.dates.map(
+      (date) => new Date(`${date.date} ${date.start_time}`),
+    ) ?? []
+  );
+}
