40160ebfa461e392f4fcad23bc303edd61041722
Josefin Wall <josefin1010wall@gmail.com>
2026-07-19 15:30:50 +0200

    Add scroll property. Remove unused functionality.
    - Pass scroll property through to Link,
    - Remove unused functionality,
    - Make property names more consistent with other components.

--- components/button.tsx
+++ components/button.tsx
@@ -4,26 +4,21 @@ import Link from "next/link";
 interface ButtonProps {
   href: string;
   children: ReactNode;
-  classes?: string;
-  isDisabled?: boolean;
+  scroll?: boolean;
+  className?: string;
 }
 
 export default function Button({
   href,
   children,
-  classes,
-  isDisabled = false,
+  className,
+  scroll = true,
 }: ButtonProps) {
-  return isDisabled ? (
-    <div
-      className={`relative z-10 font-display font-bold my-auto p-4 border rounded-2xl ${classes ?? ""}`}
-    >
-      {children}
-    </div>
-  ) : (
+  return (
     <Link
       href={href}
-      className={`relative z-10 font-display font-bold hover:ring-2 hover:ring-pink-50 focus-visible:ring-4 focus-visible:ring-pink-50 my-auto p-4 border rounded-2xl ${classes ?? ""}`}
+      scroll={scroll}
+      className={`relative z-10 font-display font-bold hover:ring-2 hover:ring-pink-50 focus-visible:ring-4 focus-visible:ring-pink-50 my-auto p-4 border rounded-2xl ${className ?? ""}`}
     >
       {children}
     </Link>
